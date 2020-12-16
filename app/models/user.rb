906b57d2afc5efe6dde30021e3f198734eb62433
ariyawala <ojima574@gmail.com>
2020-12-16 20:48:39 +0900

    バリデーション with_options に書き換え

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  validates :password,        format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
-  validates :last_name,       presence: true, format: { with: /\A[ぁ-ゔァ-ヴ-龥々]+\z/, message: 'に全角文字を使用してください' }
-  validates :first_name,      presence: true, format: { with: /\A[ぁ-ゔァ-ヴ-龥々]+\z/, message: 'に全角文字を使用してください' }
-  validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヴ]+\z/, message: 'に全角カナを使用してください' } 
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヴ]+\z/, message: 'に全角カナを使用してください' }
-  validates :birthday,        presence: true
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は6文字以上かつ英数字をそれぞれ含めてください" }
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ-龥々]+\z/, message: 'に全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヴ]+\z/, message: 'に全角カナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
 
 
 end
