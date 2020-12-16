ebd223361bb56cf6ffa47aebbb01a10426c363e2
ariyawala <ojima574@gmail.com>
2020-12-16 13:12:24 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  validates :last_name,       presence: true
+  validates :first_name,      presence: true
+  validates :last_name_kana,  presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday,        presence: true
+
+
+
+  t.string :nickname,           null: false
+  t.string :last_name,          null: false
+  t.string :first_name,         null: false
+  t.string :last_name_kana,     null: false
+  t.string :first_name_kana,    null: false
+  t.date :birthday,             null: false
+
 end
