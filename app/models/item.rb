1086547333c5bf68bc9fa26fe756f005440cb14f
ariyawala <ojima574@gmail.com>
2020-12-17 16:42:31 +0900

    Prefectureモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,13 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
 
-  validates :category_id,:condition,  numericality: { other_than: 1 }
+  with_options presence: true, numericality: { other_than: 1 }
+    validates :category_id
+    validates :condition
+    validates :shipping_charge
+    validates :prefecture
+  end
 end
