0c0f092f20e7a86c49a9e069db97a576694a631a
ariyawala <ojima574@gmail.com>
2020-12-21 16:11:17 +0900

    アクション定義の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 before_action :authenticate_user!, only: [:new, :edit]
-before_action :find_item, only: [:show, :edit, :update]
+before_action :find_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -23,9 +23,11 @@ before_action :find_item, only: [:show, :edit, :update]
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else 
+      render :show
+    end
   end
 
   def edit
