371df2cc0a8a64edea3d872ab1452b0c39dca371
ariyawala <ojima574@gmail.com>
2020-12-23 13:44:25 +0900

    アクション定義

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,38 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase = Purchase.new
+    redirect_to root_path if @item.order.present? || @item.user_id == current_user.id
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase = Purchase.new(purchase_params)
+    if @purchase.valid?
+      pay_item
+      @purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:zip, :prefecture_id, :city, :address, :building, :phone_no, :order_id).merge(
+      user_id: current_user.id, item_id: @item.id, token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
