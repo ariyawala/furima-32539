6ead327d569a6a243c9e1a2ad6b9481982378451
ariyawala <ojima574@gmail.com>
2021-01-05 18:33:31 +0900

    空白削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,5 +15,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
-
 end
