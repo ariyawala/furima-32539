fb5767aafecad241e0a191734d39ff120b5b39b3
ariyawala <ojima574@gmail.com>
2021-01-12 23:07:15 +0900

    税率を変数に変更

--- app/javascript/items_price.js
+++ app/javascript/items_price.js
@@ -3,8 +3,9 @@ if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener('input',() => {
     const inputValue = priceInput.value;
+    const tax = 0.1
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1);
+    addTaxDom.innerHTML = Math.floor(priceInput.value * tax);
     const profitDom = document.getElementById("profit")
     profitDom.innerHTML = Math.floor(priceInput.value * 0.9);
   });
